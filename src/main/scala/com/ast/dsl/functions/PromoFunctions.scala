f4b1cdab85cc033a864f3b88bbfa653cb131fccb
Arun Sethia <sethia.arun@gmail.com>
2015-12-26 22:06:44 +0530

    Update PromoFunctions.scala
    removed VarList and StringList to extend from Expr

--- src/main/scala/com/ast/dsl/functions/PromoFunctions.scala
+++ src/main/scala/com/ast/dsl/functions/PromoFunctions.scala
@@ -1,6 +1,5 @@
 package com.ast.dsl.functions
 
-
 /**
  * list of case classes used for retail promotion definition
  */
@@ -15,18 +14,18 @@ trait PromoFunctions {
   case class Promotion(rules:List[Rule],promo:Promo)
   //promo case class
   case class Promo(v1:Var) extends Expr
-  //variable case class 
+  //variable case class
   case class Var(varname:String) extends Expr 
-  //variable list
-  case class VarList(varList:List[Var]) extends Expr
-  //string list
-  case class StringList(stringList:List[String]) extends Expr
+  //variable list, this should not extend from expr
+  case class VarList(varList:List[Var]) 
+  //string list, this should not extend from expr
+  case class StringList(stringList:List[String]) 
+  //sku with list of sku names
+  case class SKURule(list:StringList) extends Rule
   //sku with list of variables
   case class SKUVarRule(list:VarList) extends VarRule
   //category with list of categories
-  case class CategoryVarRule(list:VarList) extends VarRule
-  //sku with list of sku names
-  case class SKURule(list:StringList) extends Rule
-  //category with list of categories
   case class CategoryRule(list:StringList) extends Rule
+  //category with list of categories
+  case class CategoryVarRule(list:VarList) extends VarRule
 }
