76542d8ba7636f237cf43dc9eac4566f78f82b84
Arun Sethia <sethia.arun@gmail.com>
2015-12-27 12:29:36 +0530

    Update PromoFunctions.scala
    made list as generic removed StringList and VarList and added InputList

--- src/main/scala/com/ast/dsl/functions/PromoFunctions.scala
+++ src/main/scala/com/ast/dsl/functions/PromoFunctions.scala
@@ -16,16 +16,10 @@ trait PromoFunctions {
   case class Promo(v1:Var) extends Expr
   //variable case class
   case class Var(varname:String) extends Expr 
-  //variable list, this should not extend from expr
-  case class VarList(varList:List[Var]) 
-  //string list, this should not extend from expr
-  case class StringList(stringList:List[String]) 
+  //input list type of T, this should not extend from expr
+  case class InputList[T](varList:List[T]) 
   //sku with list of sku names
-  case class SKURule(list:StringList) extends Rule
-  //sku with list of variables
-  case class SKUVarRule(list:VarList) extends VarRule
+  case class SKURule[T](list:InputList[T]) extends Rule
   //category with list of categories
-  case class CategoryRule(list:StringList) extends Rule
-  //category with list of categories
-  case class CategoryVarRule(list:VarList) extends VarRule
+  case class CategoryRule[T](list:InputList[T]) extends Rule
 }
